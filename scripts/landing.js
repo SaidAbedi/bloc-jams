1224bdb6be65e5ba35b844daf5adce27f5d6b8d0
Said Abedi <abedi.designs@gmail.com>
2017-06-29 14:00:59 -0500

    Assignment 22

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,15 @@
 var pointsArray = document.getElementsByClassName('point');
 
- var animatePoints = function(points){
-    var revealPoint = function(index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-    for (var i = 0; i < points.length; i++){
-      revealPoint(i);
-    }
-  };
+var revealPoint = function(point){
+  point.style.opacity = 1;
+  point.style.transform = "scaleX(1) translateY(0)";
+  point.style.msTransform = "scaleX(1) translateY(0)";
+  point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
+var animatePoints = function(points){
+    forEach(points, revealPoint);
+};
 
   window.onload=function(){
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
@@ -18,12 +17,12 @@ var pointsArray = document.getElementsByClassName('point');
     window.addEventListener("scroll", function (event){
       if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
         animatePoints(pointsArray);
-      if (window.innerHeight > 950) {
-        animatePoints(pointsArray);
-      if (window.innerHeight > 950) {
-           animatePoints(pointsArray);
-         }
+        if (window.innerHeight > 950) {
+          animatePoints(pointsArray);
+            if (window.innerHeight > 950) {
+              animatePoints(pointsArray);
+            }
         }
-       };
-      })
-    }
+      }
+    })
+  };
